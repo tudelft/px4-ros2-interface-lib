f603bbe2af09003d429d0a6414a9058bf3050425
sstroobants <stein.stroobants@live.nl>
2025-01-30 11:22:18 +0100

    turn off rate controller in px4

--- px4_ros2_cpp/src/control/setpoint_types/experimental/torques.cpp
+++ px4_ros2_cpp/src/control/setpoint_types/experimental/torques.cpp
@@ -34,7 +34,7 @@ SetpointBase::Configuration TorqueSetpointType::getConfiguration()
 {
   Configuration config{};
   config.manual_enabled = true;
-  config.rates_enabled = true;
+  config.rates_enabled = false;
   config.attitude_enabled = true;
   config.altitude_enabled = true;
   config.climb_rate_enabled = true;
